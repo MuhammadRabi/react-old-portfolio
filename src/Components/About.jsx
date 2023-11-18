64300e84d8aa7cbe4af35eb1f6c33a2ca519c0cf
Muhammad Rabi <vonrabi07@yahoo.com>
2023-11-18 13:03:10 +0200

    fix about-section height

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,15 +1,14 @@
-import React from "react"
 import SectionTitle from "./SectionTitle"
 import coverImage from "../assets/images/bg1.webp"
 
 const About = () => {
   return (
     <section
-      className="text-white bg-darkest page-container about-us"
+      className="text-white lg:h-screen bg-darkest page-container about-us"
       id="about"
     >
       <SectionTitle title="about me" dark />
-      <div className="flex flex-col items-center justify-between mt-12 content lg:flex-row">
+      <div className="flex flex-col items-center justify-between mt-12 lg:my-24 content lg:flex-row">
         <div className="flex-1 order-2 px-4 my-20 leading-loose text-left md:px-16 about-text">
           <p>
             I like to code things from scratch, and enjoy bringing ideas to life
@@ -23,7 +22,11 @@ const About = () => {
           </p>
         </div>
         <div className="flex-1 order-1 about-image">
-          <img src={coverImage} className="object-cover rounded-sm" alt="" />
+          <img
+            src={coverImage}
+            className="object-cover rounded-sm"
+            alt="rabi-cover-image"
+          />
         </div>
       </div>
     </section>
