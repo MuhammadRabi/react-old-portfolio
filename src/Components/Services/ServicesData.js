86245b860e6df1da217df4c89ffd69e819004104
Muhammad Rabi <vonrabi07@yahoo.com>
2024-01-15 12:20:54 +0200

    update service info

--- src/Components/Services/ServicesData.js
+++ src/Components/Services/ServicesData.js
@@ -1,9 +1,9 @@
-import imageOne from "../../assets/images/icon1.png";
-import imageTwo from "../../assets/images/icon2.png";
-import imageThree from "../../assets/images/icon3.png";
-import imageFour from "../../assets/images/icon4.png";
-import imageFive from "../../assets/images/icon5.png";
-import imageSix from "../../assets/images/icon6.png";
+import imageOne from "../../assets/images/icon1.png"
+import imageTwo from "../../assets/images/icon2.png"
+import imageThree from "../../assets/images/icon3.png"
+import imageFour from "../../assets/images/icon4.png"
+import imageFive from "../../assets/images/icon5.png"
+import imageSix from "../../assets/images/icon6.png"
 
 export const services = [
   {
@@ -16,36 +16,36 @@ export const services = [
   {
     id: 2,
     image: imageTwo,
-    title: "full support",
+    title: "performance",
     description:
-      " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis maxime voluptate tempore facere blanditiis sed, sunt corrupti temporibus cum quos! Quae architecto earum velit esse?",
+      "I can enhance Core Web Vitals through meticulous optimization. Leveraging my skills in performance tuning, I ensure your website excels in user experience, speed, and search rankings.",
   },
   {
     id: 3,
     image: imageThree,
     title: "responsive design",
     description:
-      " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis maxime voluptate tempore facere blanditiis sed, sunt corrupti temporibus cum quos! Quae architecto earum velit esse?",
+      "Expert in responsive design using React, Next.js, and Shopify customization. I ensure seamless adaptability across devices, prioritizing clean code and efficient design practices. Let's create a visually stunning and accessible web experience together!",
   },
   {
     id: 4,
     image: imageFour,
-    title: "clean code",
+    title: "best practices",
     description:
-      " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis maxime voluptate tempore facere blanditiis sed, sunt corrupti temporibus cum quos! Quae architecto earum velit esse?",
+      "I maintain clean and efficient code, adhering to best practices for readability, maintainability, and optimal performance, ensuring a solid foundation for project success.",
   },
   {
     id: 5,
     image: imageFive,
-    title: "PSD to HTML",
+    title: "Figma to HTML",
     description:
-      " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis maxime voluptate tempore facere blanditiis sed, sunt corrupti temporibus cum quos! Quae architecto earum velit esse?",
+      "Proficient in converting Figma designs to HTML. I transform your visual concepts into functional, pixel-perfect websites. Let's turn your ideas into a captivating online reality.",
   },
   {
     id: 6,
     image: imageSix,
-    title: "responsive design",
+    title: "Passionate Learner",
     description:
-      " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis maxime voluptate tempore facere blanditiis sed, sunt corrupti temporibus cum quos! Quae architecto earum velit esse?",
+      "Eager to learn new things and stay ahead in the ever-evolving tech landscape. Enthusiastic about acquiring new skills to enhance my capabilities as a web developer.",
   },
-];
+]
