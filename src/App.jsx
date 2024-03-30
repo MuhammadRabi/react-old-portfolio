ba573a4d3d3dcb0f427058caadfe05250019c655
Muhammad Rabi <vonrabi07@yahoo.com>
2024-03-31 01:09:16 +0200

    add progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,23 @@ import Scroll from "./Components/UI/Scroll"
 import { lazy, Suspense, useContext } from "react"
 import Overlay from "./Components/UI/Overlay"
 import { appContext } from "./Components/context"
+import { motion, useScroll, useSpring } from "framer-motion"
+
 const Projects = lazy(() => import("./Components/Projects/Projects"))
 
 function App() {
   const { isSidebarOpen } = useContext(appContext)
+  const { scrollYProgress } = useScroll()
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 50,
+    damping: 30,
+    restDelta: 0.001,
+  })
+
   return (
     <>
       {isSidebarOpen ? <Overlay /> : ""}
+      <motion.div className="progress-bar" style={{ scaleX }} />
       <Header />
       <About />
       <Services />
